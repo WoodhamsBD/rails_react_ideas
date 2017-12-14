d16cbf3a696bd20295c2c282edf89ca22a29b037
Bryan Woodhams <Woodhamsbd@gmail.com>
2017-12-13 20:49:49 -0800

    Idea Update working - notifications not

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,8 +3,30 @@ module Api::V1
 
     # Index method - Render all Ideas as JSON
     def index
-      @ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
       render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      
+      render json: @idea
+    end
+
+    def delete
+
+    end
+
+    #Private Methods 
+    private
+    def idea_params
+      params.require(:idea).permit(:title, :body)
     end
 
     # End Class
